d395757cecee070dbc20abb87eb717dd018f192a
centrric-cint0033 <azeem.centrric@gmail.com>
2024-03-01 16:14:08 +0530

    carpage fuel api  search api integrated and bug fixed

--- lib/view/master_car_page/master_car_page.dart
+++ lib/view/master_car_page/master_car_page.dart
@@ -5,6 +5,8 @@ import 'package:flutter/material.dart';
 import 'package:enviro_mobile_application/view/loginpage/Common_widgets/widgets/common_search_widget.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 
+TextEditingController textController = TextEditingController();
+
 class MasterCarpage extends StatelessWidget {
   const MasterCarpage({Key? key}) : super(key: key);
 
@@ -98,7 +100,65 @@ class MasterCarpage extends StatelessWidget {
                 );
               }),
               const SizedBox(height: 16.0),
-              common_search_widget(),
+              Container(
+                padding: const EdgeInsets.only(right: 15.0, left: 15),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(horizontal: 7.0),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(5.0),
+                          border: Border.all(
+                            color: const Color(0XFF949494),
+                          ),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Expanded(
+                              child: TextField(
+                                onChanged: (value) {
+                                  if (value.isEmpty) {
+                                    vmcar.mastercarfunction();
+                                  } else {
+                                    vmcar.fuelsearchfunction();
+                                  }
+                                },
+                                controller: textController,
+                                style: const TextStyle(color: Colors.black),
+                                decoration: InputDecoration(
+                                  hintText: 'Search By client',
+                                  hintStyle:
+                                      const TextStyle(color: Colors.grey),
+                                  border: InputBorder.none,
+                                  suffixIcon: Observer(builder: (_) {
+                                    return GestureDetector(
+                                      onTap: () {},
+                                      child: Container(
+                                        decoration: BoxDecoration(
+                                          color: Colors.blue,
+                                          borderRadius:
+                                              BorderRadius.circular(5.0),
+                                        ),
+                                        child: const Icon(
+                                          Icons.search,
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                    );
+                                  }),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
               const SizedBox(height: 16.0),
               Observer(
                 builder: (BuildContext context) {
