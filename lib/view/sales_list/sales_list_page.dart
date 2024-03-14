2cbb107a8cc8e310d225815c2ea15323d1a70e29
centrric-cint0033 <azeem.centrric@gmail.com>
2024-03-14 11:57:54 +0530

    salesperfoemance api bug fixed

--- lib/view/sales_list/sales_list_page.dart
+++ lib/view/sales_list/sales_list_page.dart
@@ -26,7 +26,7 @@ class SalesListPage extends StatelessWidget {
                             vmsalesperformance.salespageResponse.data?.length ??
                                 0,
                         separatorBuilder: (BuildContext context, int index) {
-                          return SizedBox(height: 12.0);
+                          return const SizedBox(height: 12.0);
                         },
                         itemBuilder: (context, index) {
                           var data =
@@ -35,7 +35,8 @@ class SalesListPage extends StatelessWidget {
                             won: data?.won,
                             lost: data?.lost,
                             pending: data?.pending,
-                            // profile: data?.profile,
+                            profile: data?.profile,
+                            name: data?.name,
                             total: data?.total,
                           );
                         },
@@ -52,57 +53,107 @@ class SalesListPage extends StatelessWidget {
   }
 
   Widget _buildJobCard({
+    required String? name,
     required int? won,
     required int? lost,
     required int? pending,
     required int? total,
-    // required String? profile,
+    required String? profile,
   }) {
     return Container(
       width: double.infinity,
       child: Card(
         margin: const EdgeInsets.only(left: 16.0, right: 16),
         shape: RoundedRectangleBorder(
-          side: BorderSide(
-            color: const Color(0XFF949494),
+          side: const BorderSide(
+            color: Color(0XFF949494),
           ),
           borderRadius: BorderRadius.circular(8.0),
         ),
         color: Colors.white,
         child: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+          child: Row(
             children: [
-              Text(
-                "won: ${won ?? ''}",
-                style: const TextStyle(color: Colors.blue),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: 100,
+                    width: 100,
+                    child: profile != null
+                        ? Image.network(
+                            profile,
+                            fit: BoxFit.contain,
+                          )
+                        : null,
+                  ),
+                  const SizedBox(height: 8.0),
+                  Text(
+                    "${name ?? ''}",
+                    style: const TextStyle(color: Colors.blue),
+                  ),
+                ],
               ),
-              const SizedBox(height: 8.0),
-              // Text(
-              //   "name: $name",
-              //   style: const TextStyle(color: Colors.blue),
-              // ),
-              const SizedBox(height: 8.0),
-              Text(
-                "lost: ${lost ?? ''}",
-                style: const TextStyle(color: Colors.blue),
+              const SizedBox(width: 16.0),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        const Text(
+                          "Total Quote Won: ",
+                          style: TextStyle(color: Colors.blue),
+                        ),
+                        Text(
+                          "${won ?? ''}",
+                          style: const TextStyle(color: Colors.blue),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 8.0),
+                    Row(
+                      children: [
+                        const Text(
+                          "Total Quote Lost: ",
+                          style: TextStyle(color: Colors.blue),
+                        ),
+                        Text(
+                          "${lost ?? ''}",
+                          style: const TextStyle(color: Colors.blue),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 8.0),
+                    Row(
+                      children: [
+                        const Text(
+                          "Total Quote Pending: ",
+                          style: TextStyle(color: Colors.blue),
+                        ),
+                        Text(
+                          "${pending ?? ''}",
+                          style: const TextStyle(color: Colors.blue),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 8.0),
+                    Row(
+                      children: [
+                        const Text(
+                          "Total Quote Made: ",
+                          style: TextStyle(color: Colors.blue),
+                        ),
+                        Text(
+                          "${total ?? ''}",
+                          style: const TextStyle(color: Colors.blue),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-              const SizedBox(height: 8.0),
-              Text(
-                "pending: ${pending ?? ''}",
-                style: const TextStyle(color: Colors.blue),
-              ),
-              const SizedBox(height: 8.0),
-              Text(
-                "total: ${total ?? ''}",
-                style: const TextStyle(color: Colors.blue),
-              ),
-              const SizedBox(height: 8.0),
-              // Text(
-              //   // "profile: $profile",
-              //   style: const TextStyle(color: Colors.blue),
-              // ),
             ],
           ),
         ),
