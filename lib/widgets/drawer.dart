6fad454cad9ab75e5170c772bb6ac63d1114086a
centrric-cint0033 <azeem.centrric@gmail.com>
2024-04-01 16:44:39 +0530

    profile drawer api integrated

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -13,142 +13,159 @@ Drawer CmnDrawer(BuildContext context) {
   return Drawer(
     width: 210.w,
     child: ListView(
+      physics: NeverScrollableScrollPhysics(),
       padding: EdgeInsets.zero,
       children: <Widget>[
         const SizedBox(
-          height: 90,
+          height: 60,
         ),
         Padding(
           padding: const EdgeInsets.only(left: 35.0),
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 73.0),
-                    child: Text(
-                        vmProfile.profilepageResponse.data?.username ?? ''),
-                  ),
-                ],
-              ),
-              sized0hx05,
-              Container(
-                height: 33,
-                child: ElevatedButton(
-                    style: ButtonStyle(
-                      side: MaterialStateProperty.all(
-                          BorderSide(color: Colors.blue)),
+          child: Container(
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    SizedBox(
+                      width: 50,
+                      height: 70,
+                      child: vmProfile.profilepageResponse.data?.dp != null
+                          ? Image.network(
+                              vmProfile.profilepageResponse.data!.dp!,
+                              fit: BoxFit.cover,
+                            )
+                          : Container(), // You can replace Container() with any placeholder widget you desire
                     ),
-                    onPressed: profilenamechnge,
-                    child: Text(
-                      vmProfile.profilepageResponse.data?.permissionType ?? '',
-                      style: const TextStyle(color: Colors.black),
-                    )),
-              ),
-              const SizedBox(
-                height: 96,
-              ),
-              ListTile(
-                leading: const Icon(
-                  Icons.home,
-                  color: Colors.blue,
+                    Padding(
+                      padding: const EdgeInsets.only(left: 23.0),
+                      child: Text(
+                        vmProfile.profilepageResponse.data?.username ?? '',
+                      ),
+                    ),
+                  ],
                 ),
-                title: const Text('Home'),
-                onTap: () {
-                  Navigator.pop(context);
-                },
-              ),
-              sized0hx05,
-              ListTile(
-                leading: const Icon(
-                  Icons.monetization_on,
-                  color: Colors.blue, // Set icon color to blue
+                const SizedBox(
+                  height: 8,
                 ),
-                title: const Text('Sales'),
-                onTap: () {
-                  onsalesfunction(context);
-                },
-              ),
-              sized0hx05,
-              ListTile(
-                leading: const Icon(
-                  Icons.directions_car,
-                  color: Colors.blue, // Set icon color to blue
+                Container(
+                  height: 33,
+                  child: ElevatedButton(
+                      style: ButtonStyle(
+                        side: MaterialStateProperty.all(
+                            const BorderSide(color: Colors.blue)),
+                      ),
+                      onPressed: profilenamechnge,
+                      child: Text(
+                        vmProfile.profilepageResponse.data?.permissionType ??
+                            '',
+                        style: const TextStyle(color: Colors.black),
+                      )),
                 ),
-                title: const Text('Vehicle'),
-                onTap: () {
-                  vehiclefunction(context);
-                  // Add your functionality here
-                  Navigator.pop(context);
-                },
-              ),
-              sized0hx05,
-              ListTile(
-                leading: const Icon(
-                  Icons.location_on,
-                  color: Colors.blue, // Set icon color to blue
+                const SizedBox(
+                  height: 46,
                 ),
-                title: const Text('Site'),
-                onTap: () {
-                  // Add your functionality here
-                  Navigator.pop(context);
-                },
-              ),
-              sized0hx05,
-              ListTile(
-                leading: const Icon(
-                  Icons.local_hospital,
-                  color: Colors.blue, // Set icon color to blue
+                ListTile(
+                  leading: const Icon(
+                    Icons.home,
+                    color: Colors.blue,
+                  ),
+                  title: const Text('Home'),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
                 ),
-                title: const Text('OH&S'),
-                onTap: () {
-                  ohsfunction(context);
+                sized0hx05,
+                ListTile(
+                  leading: const Icon(
+                    Icons.monetization_on,
+                    color: Colors.blue, // Set icon color to blue
+                  ),
+                  title: const Text('Sales'),
+                  onTap: () {
+                    onsalesfunction(context);
+                  },
+                ),
+                sized0hx05,
+                ListTile(
+                  leading: const Icon(
+                    Icons.directions_car,
+                    color: Colors.blue, // Set icon color to blue
+                  ),
+                  title: const Text('Vehicle'),
+                  onTap: () {
+                    vehiclefunction(context);
+                    // Add your functionality here
+                    Navigator.pop(context);
+                  },
+                ),
+                sized0hx05,
+                ListTile(
+                  leading: const Icon(
+                    Icons.location_on,
+                    color: Colors.blue, // Set icon color to blue
+                  ),
+                  title: const Text('Site'),
+                  onTap: () {
+                    // Add your functionality here
+                    Navigator.pop(context);
+                  },
+                ),
+                sized0hx05,
+                ListTile(
+                  leading: const Icon(
+                    Icons.local_hospital,
+                    color: Colors.blue, // Set icon color to blue
+                  ),
+                  title: const Text('OH&S'),
+                  onTap: () {
+                    ohsfunction(context);
 
-                  Navigator.pop(context);
-                },
-              ),
-              sized0hx05,
-              ListTile(
-                leading: const Icon(
-                  Icons.schedule,
-                  color: Colors.blue, // Set icon color to blue
+                    Navigator.pop(context);
+                  },
                 ),
-                title: const Text('Scheduling'),
-                onTap: () {
-                  // Add your functionality here
-                  Navigator.pop(context);
-                },
-              ),
-              sized0hx05,
-              ListTile(
-                leading: const Icon(
-                  Icons.business,
-                  color: Colors.blue, // Set icon color to blue
+                sized0hx05,
+                ListTile(
+                  leading: const Icon(
+                    Icons.schedule,
+                    color: Colors.blue, // Set icon color to blue
+                  ),
+                  title: const Text('Scheduling'),
+                  onTap: () {
+                    // Add your functionality here
+                    Navigator.pop(context);
+                  },
                 ),
-                title: const Text('Intranet'),
-                onTap: () {
-                  intranetfuntion(context);
-                  // Add your functionality here
-                  Navigator.pop(context);
-                },
-              ),
-              sized0hx05,
-              ListTile(
-                leading: const Icon(
-                  Icons.people,
-                  color: Colors.blue, // Set icon color to blue
+                sized0hx05,
+                ListTile(
+                  leading: const Icon(
+                    Icons.business,
+                    color: Colors.blue, // Set icon color to blue
+                  ),
+                  title: const Text('Intranet'),
+                  onTap: () {
+                    intranetfuntion(context);
+                    // Add your functionality here
+                    Navigator.pop(context);
+                  },
                 ),
-                title: const Text('Team'),
-                onTap: () {
-                  // Add your functionality here
-                  Navigator.pop(context);
-                },
-              ),
-            ],
+                sized0hx05,
+                ListTile(
+                  leading: const Icon(
+                    Icons.people,
+                    color: Colors.blue, // Set icon color to blue
+                  ),
+                  title: const Text('Team'),
+                  onTap: () {
+                    // Add your functionality here
+                    Navigator.pop(context);
+                  },
+                ),
+              ],
+            ),
           ),
         ),
         const SizedBox(
-          height: 30,
+          height: 10,
         ),
         ListTile(
           leading: const Padding(
