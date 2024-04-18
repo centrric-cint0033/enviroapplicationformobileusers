f050a7a6ecb9c56db04e97c67bf88ed1ceb026d5
centrric-cint0033 <azeem.centrric@gmail.com>
2024-04-18 09:39:38 +0530

    wwresponsehandler added on oh snews

--- lib/view/10_shedule/jobcard_page.dart
+++ lib/view/10_shedule/jobcard_page.dart
@@ -19,83 +19,96 @@ class JobCardPage extends StatelessWidget {
         title: cmnTitleWidget('Scheduling'),
         actions: [notificationButton(context)],
       ),
-      body: Column(
-        children: [
-          const SizedBox(height: 20),
-          Container(
-            width: 450.dg,
-            height: 40,
-            color: const Color.fromARGB(31, 124, 122, 122),
-            child: const Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Text('Site Details'),
-            ),
-          ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.only(left: 19.0, right: 19),
-              child: ListView.separated(
-                physics: NeverScrollableScrollPhysics(),
-                itemCount: 18,
-                separatorBuilder: (context, index) =>
-                    const SizedBox(height: 20),
-                itemBuilder: (context, index) {
-                  return buildCardDataOrder(index);
-                },
+      body: Expanded(
+        child: Column(
+          children: [
+            const SizedBox(height: 20),
+            Container(
+              width: 450.dg,
+              height: 40,
+              color: const Color.fromARGB(31, 124, 122, 122),
+              child: const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text('Site Details'),
               ),
             ),
-          ),
-          Container(
-            width: 450.dg,
-            height: 40,
-            color: const Color.fromARGB(31, 124, 122, 122),
-            child: const Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Text('Additional informations'),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 19.0, right: 19),
+                child: ListView.separated(
+                  physics: NeverScrollableScrollPhysics(),
+                  itemCount: 14,
+                  separatorBuilder: (context, index) =>
+                      const SizedBox(height: 20),
+                  itemBuilder: (context, index) {
+                    return buildCardDataOrder(index);
+                  },
+                ),
+              ),
             ),
-          ),
-          sized0hx40
-        ],
+            Container(
+              width: 450.dg,
+              height: 40,
+              color: const Color.fromARGB(31, 124, 122, 122),
+              child: const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text('Additional informations'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 8.0),
+              child: Align(
+                  alignment: Alignment.topLeft,
+                  child: Text('Access Restrictions')),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 8.0),
+              child: Align(
+                  alignment: Alignment.topLeft,
+                  child: Text('Access Restrictions')),
+            )
+          ],
+        ),
       ),
     );
   }
 
   Widget buildCardDataOrder(int index) {
     switch (index) {
-      case 3:
+      case 1:
         return expandedRowShowsText("Site Name", "CENTRRIC");
-      case 4:
+      case 2:
         return expandedRowShowsText("Type of Waste(site profile)", "KL-02333");
-      case 5:
+      case 3:
         return expandedRowShowsText(
             "Site Address", "Kalamassery, Cochi, Kerala, India");
-      case 6:
+      case 4:
         return expandedRowShowsText("Site postcode", "82783");
-      case 7:
+      case 5:
         return expandedRowShowsText("Site Contact name", "DSD");
-      case 8:
+      case 6:
         return expandedRowShowsText("Site Contact Phone", "276563738");
-      case 9:
+      case 7:
         return expandedRowShowsText("Site Contact Mob", "897656543");
-      case 10:
+      case 8:
         return expandedRowShowsText("Site Contact Email", "AZEEM@GMAIL.COM");
-      case 11:
+      case 9:
         return expandedRowShowsText("Price", "");
-      case 12:
+      case 10:
         return expandedRowShowsText("Account Status", "");
-      case 13:
+      case 11:
         return const Text(
           "This is NOT a recurring Quote",
           style: TextStyle(
             backgroundColor: Color.fromARGB(31, 124, 122, 122),
           ),
         );
-      case 14:
+      case 12:
         return expandedRowShowsText("Date", "1-9-2022");
 
-      case 15:
+      case 13:
         return expandedRowShowsText("Sales Person", "Richargefridge");
-      case 16:
+      case 14:
         return expandedRowShowsText("Induction Type", "");
 
       default:
