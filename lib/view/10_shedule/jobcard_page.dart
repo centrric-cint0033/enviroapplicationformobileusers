bbeccd97e5617b7fbb8ec89f2a40fe52e0905f95
centrric-cint0033 <azeem.centrric@gmail.com>
2024-04-18 13:16:56 +0530

    wwresponsehandler added on oh snews

--- lib/view/10_shedule/jobcard_page.dart
+++ lib/view/10_shedule/jobcard_page.dart
@@ -19,100 +19,382 @@ class JobCardPage extends StatelessWidget {
         title: cmnTitleWidget('Scheduling'),
         actions: [notificationButton(context)],
       ),
-      body: Expanded(
+      body: SingleChildScrollView(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             const SizedBox(height: 20),
-            Container(
-              width: 450.dg,
-              height: 40,
-              color: const Color.fromARGB(31, 124, 122, 122),
-              child: const Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Text('Site Details'),
-              ),
+            _buildSectionHeader('Site Details'),
+            const SizedBox(
+              height: 20,
             ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.only(left: 19.0, right: 19),
-                child: ListView.separated(
-                  physics: NeverScrollableScrollPhysics(),
-                  itemCount: 14,
-                  separatorBuilder: (context, index) =>
-                      const SizedBox(height: 20),
-                  itemBuilder: (context, index) {
-                    return buildCardDataOrder(index);
-                  },
+            Padding(
+              padding: const EdgeInsets.only(left: 14.0),
+              child: expandedRowShowsText("Site Name", "CENTRRIC"),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 14.0),
+              child: expandedRowShowsText(
+                  "Type of Waste(site \n profile)", "KL-02333"),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 14.0),
+              child: expandedRowShowsText(
+                  "Site Address", "Kalamassery, Cochi, Kerala, India"),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 14.0),
+              child: expandedRowShowsText("Site postcode", "82783"),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 14.0),
+              child: expandedRowShowsText("Site Contact name", "DSD"),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 14.0),
+              child: expandedRowShowsText("Site Contact Phone", "276563738"),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 14.0),
+              child: expandedRowShowsText("Site Contact Mob", "897656543"),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 14.0),
+              child:
+                  expandedRowShowsText("Site Contact Email", "AZEEM@GMAIL.COM"),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 14.0),
+              child: expandedRowShowsText("Price", ""),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 14.0),
+              child: expandedRowShowsText("Account Status", ""),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const SizedBox(height: 10),
+            const Padding(
+              padding: EdgeInsets.only(left: 14.0),
+              child: Text(
+                "This is NOT a recurring Quote",
+                style: TextStyle(
+                  backgroundColor: Color.fromARGB(31, 124, 122, 122),
                 ),
               ),
             ),
-            Container(
-              width: 450.dg,
-              height: 40,
-              color: const Color.fromARGB(31, 124, 122, 122),
-              child: const Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Text('Additional informations'),
-              ),
+            const SizedBox(
+              height: 14,
             ),
             Padding(
-              padding: const EdgeInsets.only(left: 8.0),
-              child: Align(
-                  alignment: Alignment.topLeft,
-                  child: Text('Access Restrictions')),
+              padding: const EdgeInsets.only(left: 14.0),
+              child: expandedRowShowsText("Date", "1-9-2022"),
+            ),
+            const SizedBox(
+              height: 20,
             ),
             Padding(
-              padding: const EdgeInsets.only(left: 8.0),
-              child: Align(
-                  alignment: Alignment.topLeft,
-                  child: Text('Access Restrictions')),
-            )
+              padding: const EdgeInsets.only(left: 14.0),
+              child: expandedRowShowsText("Sales Person", "Richargefridge"),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 14.0),
+              child: expandedRowShowsText("Induction Type", ""),
+            ),
+            const SizedBox(height: 20),
+            _buildSectionHeader('Additional Information'),
+            const SizedBox(
+              height: 20,
+            ),
+            _buildInfoText("Access Restrictions"),
+            const SizedBox(
+              height: 20,
+            ),
+            _buildSectioncontainer('ffrthjukl'),
+            const SizedBox(
+              height: 20,
+            ),
+            _buildInfoText("TC required | comments"),
+            const SizedBox(
+              height: 20,
+            ),
+            _buildSectioncontainer('ffrthjukl'),
+            const SizedBox(
+              height: 20,
+            ),
+            _buildInfoText("Purchaseorder | comments"),
+            const SizedBox(
+              height: 20,
+            ),
+            _buildSectioncontainer(''),
+            const SizedBox(
+              height: 20,
+            ),
+            _buildInfoText("Manifest | comments"),
+            const SizedBox(
+              height: 20,
+            ),
+            _buildSectioncontainer(''),
+            const SizedBox(
+              height: 20,
+            ),
+            _buildInfoText("Additional images"),
+            const SizedBox(
+              height: 20,
+            ),
+            _buildSectioncontainer(''),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 17.0),
+              child: expandedRowShowsText("Data from Required", ""),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 17.0),
+              child: expandedRowShowsText("AccessHieght", ""),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 17.0),
+              child: expandedRowShowsText("KeyRequired", "false"),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 17.0),
+              child: expandedRowShowsText("PitDistance from truck", ""),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 17.0),
+              child: expandedRowShowsText("Water tap location", ""),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 17.0),
+              child: expandedRowShowsText("Gurney REquired", "True"),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 17.0),
+              child: expandedRowShowsText("Confined space \n required", "No"),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 17.0),
+              child: expandedRowShowsText("Number of trucks", ""),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 17.0),
+              child: expandedRowShowsText("Estimated job duration", "adddada"),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 17.0),
+              child: expandedRowShowsText("Besttime for service", "Cefgtyhjk"),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 17.0),
+              child: expandedRowShowsText("SpecificPPE", "NO"),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 17.0),
+              child: expandedRowShowsText("Weigh bridge required", "No"),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 17.0),
+              child: expandedRowShowsText(
+                  "Number and type of \n pallets to be exchanged", "sdfghj"),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 17.0),
+              child:
+                  expandedRowShowsText("Safer data sheet \n required", "yes"),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 17.0),
+              child: expandedRowShowsText("chemist approval", "yes"),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 17.0),
+              child: expandedRowShowsText("Additional information", ""),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            _buildSectioncontainer('ffrthjukl'),
+            const SizedBox(
+              height: 20,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 17.0),
+              child: expandedRowShowsText("Capacity", "swjiisws"),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 17.0),
+              child: expandedRowShowsText("Barcode", "aaaxas"),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 17.0),
+              child: expandedRowShowsText("Job status", ""),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 17.0),
+              child: expandedRowShowsText("Pit Location", ""),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            _buildSectioncontainer('ffrthjukl'),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 17.0),
+              child: expandedRowShowsText("Smoke alarm", "sdsds"),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
           ],
         ),
       ),
     );
   }
 
-  Widget buildCardDataOrder(int index) {
-    switch (index) {
-      case 1:
-        return expandedRowShowsText("Site Name", "CENTRRIC");
-      case 2:
-        return expandedRowShowsText("Type of Waste(site profile)", "KL-02333");
-      case 3:
-        return expandedRowShowsText(
-            "Site Address", "Kalamassery, Cochi, Kerala, India");
-      case 4:
-        return expandedRowShowsText("Site postcode", "82783");
-      case 5:
-        return expandedRowShowsText("Site Contact name", "DSD");
-      case 6:
-        return expandedRowShowsText("Site Contact Phone", "276563738");
-      case 7:
-        return expandedRowShowsText("Site Contact Mob", "897656543");
-      case 8:
-        return expandedRowShowsText("Site Contact Email", "AZEEM@GMAIL.COM");
-      case 9:
-        return expandedRowShowsText("Price", "");
-      case 10:
-        return expandedRowShowsText("Account Status", "");
-      case 11:
-        return const Text(
-          "This is NOT a recurring Quote",
-          style: TextStyle(
-            backgroundColor: Color.fromARGB(31, 124, 122, 122),
+  Widget _buildSectionHeader(String title) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      width: double.infinity,
+      height: 40,
+      color: const Color.fromARGB(31, 124, 122, 122),
+      alignment: Alignment.centerLeft,
+      child: Text(
+        title,
+        // style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+
+  Widget _buildSectioncontainer(String title) {
+    return Container(
+      height: 70,
+      width: double.infinity,
+      color: const Color.fromARGB(31, 124, 122, 122),
+      alignment: Alignment.centerLeft,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text(
+          title,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildExpandedRow(String label, String value) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            label,
+            style: const TextStyle(fontWeight: FontWeight.bold),
           ),
-        );
-      case 12:
-        return expandedRowShowsText("Date", "1-9-2022");
+          Text(value),
+        ],
+      ),
+    );
+  }
 
-      case 13:
-        return expandedRowShowsText("Sales Person", "Richargefridge");
-      case 14:
-        return expandedRowShowsText("Induction Type", "");
-
-      default:
-        return Container();
-    }
+  Widget _buildInfoText(String text) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+      child: Text(text),
+    );
   }
 }
